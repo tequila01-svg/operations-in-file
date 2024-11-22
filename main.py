8e168755fd71551c1110afaced025e919c083dae
tequila01-svg <onifadeayobami008@gmail.com>
2024-11-22 17:42:16 +0100

    creating and deleting a file

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-with open ('codingal.txt','w') as file:
-    file.write("hello there i see you")
-file.close()
+#with open ('codingal.txt','w') as file:
+ #   file.write("hello there i see you")
+#file.close()
 
-with open('codingal.txt','r') as file:
-    data=file.readlines()
-    print('words in the file are....')
-    for line in data:
-        word=line.split()
-        print(word)
-file.close()
+#with open('codingal.txt','r') as file:
+  #  data=file.readlines()
+   # print('words in the file are....')
+    #for line in data:
+     #   word=line.split()
+      #  print(word)
+#file.close()
+
+new_file=open('new_file.txt','x')
+new_file.close()
+
+import os
+print('checking if my_file exist...')
+if os.path.exists('my_file.txt'):
+    os.remove('my_file.txt')
+else:
+    print('file does not exist')
+    
+my_file=open('my_file.txt','w')
+my_file.write("hello i am a new file")
+my_file.close()
+
+os.remove("codingal.txt")
